da28f30a2ee4eb3e5dc165d75e90c29096b30fb3
kmilanbanda <kmilanbanda@gmail.com>
2026-02-28 16:14:44 -0800

    Add simple Entry model and admin registration

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Entry
 
-# Register your models here.
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'updated')
+    list_filter = ('pub_date',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'pub_date'
--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, help_text="Auto-generated from title for clean URLs")
+    content = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-pub_date']  # Newest first
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        # We'll use this for RSS links and future detail pages
+        return reverse('feed:entry_detail', kwargs={'slug': self.slug})
